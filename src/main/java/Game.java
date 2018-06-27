3f736b0b03e947049a2d79042d4dac7ca49e5503
James Henderson <hendersonjamesalexander@gmail.com>
2018-06-27 14:12:53 +0100

    almost done

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+
 public class Game {
+
+    private ArrayList<Player> players;
+    Deck deck;
+
+    public Game(){
+        this.players = new ArrayList<>();
+        deck = new Deck();
+        deck.newDeck();
+    }
+
+    public int getPlayers(){
+        return players.size();
+    }
+
+    public void addPLayer(Player player){
+        players.add(player);
+    }
+
+    public void dealSingleCard(Player player, Deck deck){
+        Card card = deck.removeCard();
+        player.getDealt(card);
+    }
+
+    public void dealRound(){
+        for (Player player : players){
+            Card card = deck.removeCard();
+            player.getDealt(card);
+        }
+
+    }
+
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,54 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTest {
+
+    Deck deck;
+    Player player1;
+    Player player2;
+    Hand hand1;
+    Hand hand2;
+    Game game;
+
+    @Before
+    public void before(){
+        deck = new Deck();
+        hand1 = new Hand();
+        hand2 = new Hand();
+        player1 = new Player("A", hand1);
+        player2 = new Player("B", hand2);
+        game = new Game();
+    }
+
+    @Test
+    public void hasZeroPlayers(){
+        assertEquals(0, game.getPlayers());
+    }
+
+    @Test
+    public void gameDeal(){
+        deck.newDeck();
+        game.dealSingleCard(player1, deck);
+        System.out.println(player1.getScore());
+        System.out.println(player1.getScore());
+    }
+
+    @Test
+    public void canAddPlayer(){
+        game.addPLayer(player1);
+        game.addPLayer(player2);
+        assertEquals(2, game.getPlayers());
+    }
+
+    @Test
+    public void canDealRound(){
+        deck.newDeck();
+        game.addPLayer(player1);
+        game.addPLayer(player2);
+        game.dealRound();
+
+    }
 }
+
